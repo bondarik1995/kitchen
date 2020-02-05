29711c76d6a4435c22fe67090a9c79453efa4cc8
mariya.bondarik <Mariya.Bondarik@fortegrp.com>
2020-02-05 17:24:31 +0300

    set dynamic current year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import cx from "classnames";
 
 import { COPYRIGHT, DEVELOPERS } from "constants/Footer/";
@@ -6,21 +6,33 @@ import { COPYRIGHT, DEVELOPERS } from "constants/Footer/";
 import styles from "./Footer.module.scss";
 import stylesApp from "styles/_base.scss";
 
-function Footer() {
-  return (
-    <footer>
-      <div className={cx(stylesApp["container"])}>
-        <div className={cx(styles["footer"])}>
-          <div className={cx(styles["footer-item"])}>
-            <p>2020 {COPYRIGHT}</p>
-          </div>
-          <div className={cx(styles["footer-item"])}>
-            <p>{DEVELOPERS}</p>
+class Footer extends Component {
+  state = {};
+
+  componentDidMount() {
+    this.setState({ currentYear: new Date().getFullYear() });
+  }
+
+  render() {
+    const { currentYear } = this.state;
+
+    return (
+      <footer>
+        <div className={cx(stylesApp["container"])}>
+          <div className={cx(styles["footer"])}>
+            <div className={cx(styles["footer-item"])}>
+              <p>
+                {currentYear} {COPYRIGHT}
+              </p>
+            </div>
+            <div className={cx(styles["footer-item"])}>
+              <p>{DEVELOPERS}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
-  );
+      </footer>
+    );
+  }
 }
 
 export default Footer;
